7bb63665859805b62d287196c706fdb51aeed4c8
Emily Halleran <emily@blueroserevival.com>
2017-04-13 11:17:05 -0600

    Add Photo

--- epic/persona.php
+++ epic/persona.php
@@ -9,6 +9,7 @@
 	<body>
 		<h1>Persona</h1>
 		<main>
+			<img src="img/max.jpg" alt="Maxie">
 			<p>⦁	23 year old man named Max who has allergies and needs natural shaving products, soaps, and natural clothing and watches. Likes kraft beer. He has decent technological knowledge as he is a student learning programming. Has a macbook pro.
 			</p>
 			<table>
