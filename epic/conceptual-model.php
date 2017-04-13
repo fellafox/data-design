4b89adb4a252ce591f81460f7fd4cd9a89419cbe
Emily Halleran <emily@blueroserevival.com>
2017-04-13 11:58:31 -0600

    Add Photo

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -8,25 +8,31 @@
 	<body>
 		<h1>Conceptual-Model</h1>
 		<main>
-<h2>Main Entity: Profile</h2>
-			<h2>Attributes:</h2>
+<h2>Main Entities and Attributes</h2>
+			<h3>Profile</h3>
 			<ol>
-				<li>salt</li>
-				<li>hash</li>
-				<li>validationTokein</li>
-				<li>profileId</li>
-				<li>productId</li>
-				<li>favoriteDate</li>
-				<li>productPrice</li>
-				<li>productName</li>
-				<li>productDescription</li>
-				<li>foreignKeyprofile</li>
-				<li>foreignKeyproduct</li>
+				<li>profileId (primary key)</li>
+				<li>profileSalt (for account password)</li>
+				<li>profileHash {for account password)</li>
+				<li>profileActivationToken (for account verification)</li>
+				<li>profileEmail</li>
 			</ol>
+			<h3>Product</h3>
+			<ol>
+			<li>productProfileId (primary key)</li>
+			<li>productId (primary key)</li>
+			<li>productDescription</li>
+			</ol>
+			<h3>Favorite</h3>
+		<ol>
+			<li>favoriteProfileId (foreign key)</li>
+			<li>favoriteProductId (foreign key)</li>
+			<li>favoriteDate</li>
+		</ol>
 			<h2>Relationships:</h2>
 			<ol>
-			<li>many users can favorite many products</li>
-			<li>many products can be favorited by many users</li>
+			<li>many users can favorite many products (m t n)</li>
+			<li>many products can be favorited by many users (m to n)</li>
 		</ol>
 		</main>
 	</body>
