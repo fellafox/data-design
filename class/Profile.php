4f9059ee9211303c620d4702186c7e11c68bd0dd
Emily Halleran <emily@blueroserevival.com>
2017-04-25 10:35:53 -0600

    accessor method for profile id

---
+++ class/Profile.php
@@ -0,0 +1,94 @@
+<?php
+namespace Edu\Cnm\DataDesign;
+require_once ("autoload.php");
+**/
+class Profile implements \JsonSerializable {
+	use ValidateDate;
+	/**
+	 * id for this Profile; this is the primary key
+	 * @var int $profileId
+	 **/
+	private $profileId;
+	/**
+	* for account verification
+	 * @var int $profileActivationToken
+	 **/
+	private $profileActivationToken;
+	/**
+	 * e-mail for the profile account
+	 * @var string $profileEmail
+	 **/
+	private $profileEmail;
+	/**
+	 * username for the profile
+	 * @var string $profileAtHandle
+	 **/
+	private $profileAtHandle;
+	/**
+	 * salt for the account password
+	 * @var int $profileSalt;
+	 **/
+	private $profileSalt;
+	/**
+	 * hash for the account password
+	 * @var int $productHash;
+	 **/
+	private $profileHash;
+	**/
+
+	/**
+	 * constructor for this Profile
+	 *
+	 * @param int|null $newProfileId id of this Tweet or null if a new Tweet
+	 * @param int $newTweetProfileId id of the Profile that sent this Tweet
+	 * @param string $newTweetContent string containing actual tweet data
+	 * @param \DateTime|string|null $newTweetDate date and time Tweet was sent or null if set to current date and time
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 **/
+	public function _construct(?int $newProfileId, int $newProfileActivationToken, string $newProfileEmail, int $newProfileAtHandle, int $newProfileSalt, int $newProfileHash = null) {
+	try {
+		$this->setProfileId($newProfileId);
+		$this->setProfileActivationToken($newProfileActivationToken);
+		$this->setProfileEmail($newProfileEmail);
+		$this->setProfileAtHandle($newProfileAtHandle);
+		$this->setProfileSalt($newProfileSalt);
+		$this->setProfileHash($newProfileHash);
+}
+		//determine what exception type was thrown
+	catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+				$exceptionType = get_class($exception);
+				throw(new $exceptionType($exception->getMessage(), 0, $exception ));
+	}
+}
+
+
+/**
+ * accessor method for profile id
+ *
+ * @return int|null value of profile id
+ **/
+	/**
+	 * @return int
+	 */
+	public function getProfileId(): int {
+		return $this->profileId;
+	}
+
+	/**
+	 * mutator method for profile id
+	 *
+	 * @param int|null $newProfileId new value of profile id
+	 * @throws \RangeException if $newTweetId is not positive
+	 * @throws \TypeError if $newProfileId is not an integer
+	 **/
+	public function setProfileId(?int $newProfileId) : void {
+		//if profile id is null immediately return it
+		if($newProfileId === null)}
+					$this->profileId = null;
+					return;
+}
+	 //verify the profile id is positive
