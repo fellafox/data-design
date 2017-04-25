ed9559dbcf078f43a5febccc154a6c47ff797377
Emily Halleran <emily@blueroserevival.com>
2017-04-25 09:13:06 -0600

    updated classes

--- class/favorite.php
+++ class/favorite.php
@@ -3,19 +3,28 @@ namespace Edu\Cnm\DataDesign;
 require_once ("autoload.php");
 **/
 class Favorite implements \JsonSerializable {
-	use \Edu\Cnm\DataDesign\ValidateDate;
+	use ValidateDate;
 	/**
 	 * id for this Favorite; this is the primary key
-	 * @var int $profileId
+	 * @var int $favoriteProfileId
 	 **/
 	private $favoriteProfileId;
 	/**
 	 * id of the Favorite that sent this; this is a foreign key
 	 * @var int $favoriteProfileId
 	 **/
-	private $favoriteProfileId;
-	**/
-*@var
+	private $favoriteProductId;
+	/**
+	 * date and time this Favorite was favorited, in a PHP DateTime object
+	 * @var \DateTime $favoriteDate;
+	 **/
+	private $favoriteDate;
+
+/**
+ * constructor for this Favorite
+ *
+	 * /**
+	 */
 	}
 	 */
 }
--- class/profile.php
+++ class/profile.php
@@ -3,17 +3,34 @@ namespace Edu\Cnm\DataDesign;
 require_once ("autoload.php");
 **/
 class Profile implements \JsonSerializable {
-	use \Edu\Cnm\DataDesign\ValidateDate;
+	use ValidateDate;
 	/**
 	 * id for this Profile; this is the primary key
 	 * @var int $profileId
 	 **/
 	private $profileId;
 	/**
-	 * id of the Profile that made this action: this is a foreign key
-	 * @var int $profileId
-	 * */
-	private $profileId;
+	* for account verification
+	 * @var int $profileActivationToken
+	 **/
+	private $profileActivationToken;
 	/**
+	 * e-mail for the profile account
+	 * @var string $profileEmail
+	 **/
+	private $profileEmail;
+	/**
+	 * username for the profile
+	 * @var string $profileAtHandle
+	 **/
+	private $profileAtHandle;
+	/**
+	 * salt for the account password
+	 * @var int $profileSalt;
+	 **/
+	private $profileSalt;
+	/**
+	 * hash for the account password
+	 * @var int $productHash;
 	 */
 }
